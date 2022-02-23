09cf32e917667736288aed1bafb39e1a8d7878ce
Cynthia Teeters <cynthiateeters@gmail.com>
2022-02-22 23:29:14 -0500

    Add Object.freeze() to test that arrays are not mutated

--- tests/arrays-03.test.js
+++ tests/arrays-03.test.js
@@ -20,16 +20,18 @@ test('addUpArrayElements', () => {
 
 test('makeSquareNumbers', () => {
   const sampleArray = [1, 2, 3, 4, 5];
+  const frozenArray = Object.freeze(sampleArray);
   const expectedArray = [1, 4, 9, 16, 25];
-  expect(makeSquareNumbers(sampleArray)).toEqual(expectedArray);
-  expect(makeSquareNumbers(sampleArray)).not.toBe(sampleArray);
+  expect(makeSquareNumbers(frozenArray)).toEqual(expectedArray);
+  expect(makeSquareNumbers(frozenArray)).not.toBe(sampleArray);
 });
 
 test('filterForLargeWords', () => {
   const sampleArray = ['a', 'big', 'black', 'bear', 'is', 'running'];
+  const frozenArray = Object.freeze(sampleArray);
   const expectedArray = ['black', 'bear', 'running'];
-  expect(filterForLargeWords(sampleArray, 3)).toEqual(expectedArray);
-  expect(filterForLargeWords(sampleArray, 3)).not.toBe(sampleArray);
+  expect(filterForLargeWords(frozenArray, 3)).toEqual(expectedArray);
+  expect(filterForLargeWords(frozenArray, 3)).not.toBe(sampleArray);
 });
 
 test('findTheFirstEvenNumber', () => {
@@ -41,55 +43,63 @@ test('findTheFirstEvenNumber', () => {
 
 test('sortCaseSensitive', () => {
   const sampleArray = ['A', 'big', 'black', 'bear', 'is', 'running'];
+  const frozenArray = Object.freeze(sampleArray);
   const expectedArray = ['A', 'bear', 'big', 'black', 'is', 'running'];
-  expect(sortCaseSensitive(sampleArray)).toEqual(expectedArray);
-  expect(sortCaseSensitive(sampleArray)).not.toBe(sampleArray);
+  expect(sortCaseSensitive(frozenArray)).toEqual(expectedArray);
+  expect(sortCaseSensitive(frozenArray)).not.toBe(sampleArray);
 });
 
 test('makeTheString', () => {
   const sampleArray = ['A', 'big', 'black', 'bear', 'is', 'running'];
+  const frozenArray = Object.freeze(sampleArray);
   const expectedString = 'A big black bear is running';
-  expect(makeTheString(sampleArray, ' ')).toBe(expectedString);
-  expect(makeTheString(sampleArray, ' ')).not.toBe(sampleArray);
+  expect(makeTheString(frozenArray, ' ')).toBe(expectedString);
+  expect(makeTheString(frozenArray, ' ')).not.toBe(sampleArray);
 });
 
 test('reverseTheArray', () => {
   const sampleArray = ['A', 'big', 'black', 'bear', 'is', 'running'];
+  const frozenArray = Object.freeze(sampleArray);
   const expectedArray = ['running', 'is', 'bear', 'black', 'big', 'A'];
-  expect(reverseTheArray(sampleArray)).toEqual(expectedArray);
-  expect(reverseTheArray(sampleArray)).not.toBe(sampleArray);
+  expect(reverseTheArray(frozenArray)).toEqual(expectedArray);
+  expect(reverseTheArray(frozenArray)).not.toBe(sampleArray);
 });
 
 test('makeMirrorArray', () => {
   let sampleArray = [1, 2];
+  const frozenArray = Object.freeze(sampleArray);
   let expectedArray = [1, 2, 1];
-  expect(makeMirrorArray(sampleArray)).toEqual(expectedArray);
-  expect(makeMirrorArray(sampleArray)).not.toBe(sampleArray);
+  expect(makeMirrorArray(frozenArray)).toEqual(expectedArray);
+  expect(makeMirrorArray(frozenArray)).not.toBe(sampleArray);
   sampleArray = [1, 4, 8, 10, -21];
+  const frozenArray2 = Object.freeze(sampleArray);
   expectedArray = [1, 4, 8, 10, -21, 10, 8, 4, 1];
-  expect(makeMirrorArray(sampleArray)).toEqual(expectedArray);
-  expect(makeMirrorArray(sampleArray)).not.toBe(sampleArray);
+  expect(makeMirrorArray(frozenArray2)).toEqual(expectedArray);
+  expect(makeMirrorArray(frozenArray2)).not.toBe(sampleArray);
 });
 
 test('dropRight', () => {
   const sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const frozenArray = Object.freeze(sampleArray);
   const expectedArray = [1, 2, 3, 4, 5, 6, 7];
-  expect(dropRight(sampleArray, 3)).toEqual(expectedArray);
-  expect(dropRight(sampleArray, 3)).not.toBe(sampleArray);
+  expect(dropRight(frozenArray, 3)).toEqual(expectedArray);
+  expect(dropRight(frozenArray, 3)).not.toBe(sampleArray);
 });
 
 test('dropLeft', () => {
   const sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const frozenArray = Object.freeze(sampleArray);
   const expectedArray = [5, 6, 7, 8, 9, 10];
-  expect(dropLeft(sampleArray, 4)).toEqual(expectedArray);
-  expect(dropLeft(sampleArray, 4)).not.toBe(sampleArray);
+  expect(dropLeft(frozenArray, 4)).toEqual(expectedArray);
+  expect(dropLeft(frozenArray, 4)).not.toBe(sampleArray);
 });
 
 test('checkArrayForValue', () => {
   const sampleArray = [1, true, 3, NaN];
-  expect(checkArrayForValue(sampleArray, 1)).toBe(true);
-  expect(checkArrayForValue(sampleArray, true)).toBe(true);
-  expect(checkArrayForValue(sampleArray, 3)).toBe(true);
-  expect(checkArrayForValue(sampleArray, NaN)).toBe(true);
-  expect(checkArrayForValue(sampleArray, 'a')).toBe(false);
+  const frozenArray = Object.freeze(sampleArray);
+  expect(checkArrayForValue(frozenArray, 1)).toBe(true);
+  expect(checkArrayForValue(frozenArray, true)).toBe(true);
+  expect(checkArrayForValue(frozenArray, 3)).toBe(true);
+  expect(checkArrayForValue(frozenArray, NaN)).toBe(true);
+  expect(checkArrayForValue(frozenArray, 'a')).toBe(false);
 });
